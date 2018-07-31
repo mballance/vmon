013451ee4064320955704bec8a3337102b043e3a
Matthew Ballance <matt.ballance@gmail.com>
2018-07-31 07:30:25 -0700

    update non-virtual-interface interfaces to VMON monitor

--- src/client/sv/vmon_client_api_pkg.sv
+++ src/client/sv/vmon_client_api_pkg.sv
@@ -14,7 +14,7 @@ package vmon_client_api_pkg;
 `ifdef HAVE_HDL_VIRTUAL_INTERFACE
 	class vmon_m2h_api;
 		virtual task write(
-			input byte unsigned		data[64],
+			longint unsigned		data,
 			input int unsigned		size);
 			// TODO: must be implemented 
 		endtask
